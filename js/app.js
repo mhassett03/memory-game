5e1d572d0d9c3855e8a79d49482c5dd5694bc281
mhassett03 <33911126+mhassett03@users.noreply.github.com>
2017-11-24 15:26:10 -0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,39 +1,39 @@
 // Set-up of the symbols list, etc.
-var symbols = ['diamond', 'diamond', 'paper-plane-o', 'paper-plane-o', 'bolt', 'bolt', 'cube', 'cube', 'anchor', 'anchor', 'bicycle', 'bicycle', 'bomb', 'bomb', 'leaf', 'leaf'],
-	opened = [],
-	match = 0,
-	moves = 0,
-	clicks = 0,
-	$deck = jQuery('.deck'),
-	$scorePanel = $('#score-panel'),
-	$moveNum = $('.moves'),
+let symbols = ['diamond', 'diamond', 'paper-plane-o', 'paper-plane-o', 'bolt', 'bolt', 'cube', 'cube', 'anchor', 'anchor', 'bicycle', 'bicycle', 'bomb', 'bomb', 'leaf', 'leaf'],
+	opened = [],  // Array of Card
+	match = 0, // varaible Initialization for match
+	moves = 0, // variable Initialization  for move
+	clicks = 0, // variable Initialization for Clicks
+	$deck = jQuery('.deck'),  // get html and pass it to Javascript
+	$scorePanel = $('#score-panel'),  // get html and pass it to Javascript scorepanel
+	$moveNum = $('.moves'), // same for moves
 	$starScore = $('i'),
-	$restart = $('.restart'),
+	$restart = $('.restart'), // same for restart button
 	timer;
 
 
 // Game-timer installation
-var gameTimer = () => {
-	var startTime = new Date().getTime();
+let gameTimer = () => {
+	let startTime = new Date().getTime();   // get the current time from the system
 
 	// Update the timer every second
 	timer = setInterval(() => {
 
-		var now = new Date().getTime();
+		let now = new Date().getTime();
 
 		// Find the time elapsed between now and start
-		var elapsed = now - startTime;
+		let elapsed = now - startTime;
 
 		// Calculates minutes and seconds
-		var minutes = Math.floor((elapsed % (1000 * 60 * 60)) / (1000 * 60));
-		var seconds = Math.floor((elapsed % (1000 * 60)) / 1000);
+		let minutes = Math.floor((elapsed % (1000 * 60 * 60)) / (1000 * 60));
+		let seconds = Math.floor((elapsed % (1000 * 60)) / 1000);
 
 		// Add starting '0' if seconds < 10
 		if (seconds < 10) {
 			seconds = "0" + seconds;
 		}
 
-		var currentTime = minutes + ':' + seconds;
+		let currentTime = minutes + ':' + seconds; // Current Time in the form of 10:00
 
 		// Update clock on game screen and modal
 		$(".clock").text(currentTime);
@@ -42,29 +42,30 @@ var gameTimer = () => {
 
 
 // "Test Your Memory" Game Engine with shuffled symbols
-var startGame = ()=> {
-  var cards = shuffle(symbols);
+let startGame = ()=> {
+
+	let cards = shuffle(symbols); // call the function and everything gets shuffled.
   $deck.empty();
   match = 0;
   moves = 0;
   $moveNum.text('0');
-  $starScore.removeClass('fa-star-o').addClass('fa-star');
-	for (var i = 0; i < cards.length; i++) {
+  $starScore.removeClass('fa-star-o').addClass('fa-star'); // It removes some static html and put a dynamic one after shuffling cards
+	for (let i = 0; i < cards.length; i++) {
 		$deck.append($('<li class="card"><i class="fa fa-' + cards[i] + '"></i></li>'))
 	}
-	addClkListener();
+	addClkListener(); // Click function started
 	$(".clock").text("0:00");
 };
 
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
-        temporaryValue = array[currentIndex];
+         temporaryValue = array[currentIndex];
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
@@ -77,8 +78,8 @@ function shuffle(array) {
 	Gives star-rating based on number of tries to complete the game by removing stars
 	Displays the number of tries it took to complete the game
 */
-var setRating =(moves)=> {
-	var score = 3;
+let setRating =(moves)=> {
+	let score = 3;
 	if(moves <= 12) {
 		$starScore.eq(3).removeClass('fa-star').addClass('fa-star-o');
 		score = 3;
@@ -94,31 +95,31 @@ var setRating =(moves)=> {
 
 
 
-var addClkListener = ()=> {
+let addClkListener = ()=> {
 	// Card click listener for flipping card
 	$deck.find('.card:not(".match, .open")').bind('click' , function() {
-		clicks++ ;
-		clicks == 1 ? gameTimer() :'';
-		// Check for call to happen before all dom update
+		clicks++ ; // Increment in Click
+		clicks == 1 ? gameTimer() :''; // Timer starts on first click
+		// Check for call to happen before dom update
 		if($('.show').length > 1) { return true; };
-		var $this = $(this), card = $this.context.innerHTML;
+		let $this = $(this), card = $this.context.innerHTML;
 		// Check if the player has clicked the same card
-		if($this.hasClass('open')){ return true;};
+		if ($this.hasClass('open')) { return true; };  // the player has clicked the same card
 	  $this.addClass('open show');
-		opened.push(card);
+		opened.push(card); // should add in a array where we can keep track of all elements that has been matched in the game
 		// Check the card
-		// Add/remove appropriate CSS animation classes
+		// Add and remove appropriate CSS animation classes
 	  if (opened.length > 1) {
-	    if (card === opened[0]) {
-	      $deck.find('.open').addClass('match animated infinite rubberBand');
+	    if (card === opened[0]) { // Yes
+	      $deck.find('.open').addClass('match animated infinite rubberBand'); // add the class of rubberBand that show static Layout for matched
 	      setTimeout(()=> {
-	        $deck.find('.match').removeClass('open show animated infinite rubberBand');
+	        $deck.find('.match').removeClass('open show animated infinite rubberBand'); // remove class
 	      }, 500);
-	      match++;
+	      match++; // Increment in match
 	    } else {
-	      $deck.find('.open').addClass('notmatch animated infinite wobble');
+	      $deck.find('.open').addClass('notmatch animated infinite wobble'); // if not matched add notmached wooble clas that wobble for a second
 				setTimeout(()=> {
-					$deck.find('.open').removeClass('animated infinite wobble');
+					$deck.find('.open').removeClass('animated infinite wobble'); // remove previous class
 				}, 500 / 1.5);
 	      setTimeout(()=> {
 	        $deck.find('.open').removeClass('open show notmatch animated infinite wobble');
@@ -126,15 +127,15 @@ var addClkListener = ()=> {
 	    }
 	    opened = [];
 			moves++;
-			setRating(moves);
-			$moveNum.html(moves);
+			setRating(moves); // function call for moves
+			$moveNum.html(moves); // put into the html to show on client side
 	  }
 		// If all cards are matching (8 matches) will end the "Test Your Memory" Game
 		if (match === 8) {
 			setRating(moves);
-			var score = setRating(moves).score;
+			let score = setRating(moves).score;
 			setTimeout(()=> {
-				endGame(moves, score);
+				endGame(moves, score); // end game get moves and score
 			}, 800);
 	  }
 	});
@@ -145,18 +146,20 @@ var addClkListener = ()=> {
 	Ends the "Test Your Memory" Game
 	Open popup window with final score and 'play again' option
 */
-var endGame = (moves, score) => {
-	var numberOfStars = score == 1 ? score + ' star.' :score +' stars.';
+let endGame = (moves, score) => {
+
+  var Total_Time =  $(".clock").text(); // get time from timer
+	let numberOfStars = score == 1 ? score + ' star.' :score +' stars.';
 	// SweetAlerts from https://limonte.github.io/sweetalert2/
 	swal({
 		allowEscapeKey: false,
 		allowOutsideClick: false,
 		title: 'You did it!',
-		text: 'In ' + moves + ' tries and ' + numberOfStars + '\n Great Job!',
+		text: 'In ' + Total_Time + ' and ' + numberOfStars + ' Great Job!', // text to show on congrats
 		type: 'success',
 		confirmButtonColor: '#79CCF6',
 		confirmButtonText: 'Do it again!'
-	}).then((isConfirm)=> {
+	}).then((isConfirm)=> { // click on play once again
 		if (isConfirm) {
 			clicks = 0;
 			clearInterval(timer);
